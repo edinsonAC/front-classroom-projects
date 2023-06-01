93532d72c8ee05914df7caac11a16ac35c5d450b
dianalleon <diana.kotoko115@gmail.com>
2023-06-01 12:20:34 -0500

    cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
     // API_URL: 'http://localhost:3000/api'
     // API_URL: 'https://classroomprojectsts-production.up.railway.app/api'
     API_NODE: 'https://classroomprojectsts-production-bbb6.up.railway.app/api',
-    API_SPRING: 'http://18.188.178.178:8080/api-subject'
+    API_SPRING: 'http://52.15.222.122:9000/api-subject'
 
 };
