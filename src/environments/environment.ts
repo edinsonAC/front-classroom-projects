23c96bd80d0ef65fd66fd5ab59fea3337fc75243
Edinson <edinson.antolinez@tus-cuentas.com>
2023-06-22 13:26:22 -0500

    cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,7 @@ export const environment = {
     // API_URL: 'https://classroomprojectsts-production.up.railway.app/api'
     API_NODE: 'https://classroomprojectsts-production-bbb6.up.railway.app/api',
     API_SPRING: 'http://52.15.222.122:9000/api-subject'
+    // API_SPRING: 'http://localhost:9000/api-subject'
+    // API_SPRING: 'http://18.118.199.176:8080/api-subject'
 
 };
